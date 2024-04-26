817e1ff8c3756d6cfba56b86b87a162e777768fb
Aleksei Brutov <mjbcaepio@gmail.com>
2024-04-26 16:04:08 +0200

    DTO Serializer and ObjectNormalizer

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, DTOSerializer $serializer, int $id): Response
+    {
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setPromotionId(3);
+        $lowestPriceEnquiry->setPromotionName('The Best!');
+
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
+
+        return new Response($responseContent, 200);
+    }
+
+}
