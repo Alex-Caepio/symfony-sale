8b8e46317a2328fce6624f396028a1aaad204936
Aleksei Brutov <mjbcaepio@gmail.com>
2024-05-10 18:32:49 +0200

    Another feature

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,27 +4,28 @@ namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
+use App\Filter\PromotionsFilterInterface;
 use App\Repository\ProductRepository;
 use App\Service\Serializer\DTOSerializer;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsController extends AbstractController
 {
     public function __construct(private ProductRepository $repository, private EntityManagerInterface $entityManager)
     {
-
     }
 
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(Request $request, DTOSerializer $serializer, int $id): Response
-    {
+    public function lowestPrice(
+        Request $request,
+        DTOSerializer $serializer,
+        int $id,
+        PromotionsFilterInterface $promotionsFilter
+    ): Response {
         /** @var LowestPriceEnquiry $lowestPriceEnquiry */
         $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
 
@@ -36,11 +37,7 @@ class ProductsController extends AbstractController
             date_create_immutable($lowestPriceEnquiry->getRequestDate())
         );
 
-        dd($promotions);
-//        $lowestPriceEnquiry->setDiscountedPrice(50);
-//        $lowestPriceEnquiry->setPrice(100);
-//        $lowestPriceEnquiry->setPromotionId(3);
-//        $lowestPriceEnquiry->setPromotionName('The Best!');
+        $modifiedInquiry = $promotionsFilter->apply($lowestPriceEnquiry);
 
         $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
 
